6b0ecabbf7b21448622afd0b343ea8ae0595aa7c
DavyKandido <davicandidopucminas@gmail.com>
2024-08-12 18:57:57 -0300

    c

--- Testes_e_Estudos/teste.java
+++ Testes_e_Estudos/teste.java
@@ -6,12 +6,15 @@ class teste{
 
         int n = sc.nextInt();
         int a = 1;
+        int mult = 0;
 
         for(int i = n; i>0; i/=2){
             a*=2;
+            mult++;
         }
         
         System.out.println(a);
+        System.out.println(mult);
 
         sc.close();
     }
